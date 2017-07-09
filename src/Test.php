d8cfc8f193e10fea16f3176a01104f5b2060f1e6
xieming-mac <xieming@youyoumob.com>
2017-07-09 16:43:30 +0800

    add new file

---
+++ src/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xieming
+ * Date:2017-07-09 16:40
+ * File:Test.php
+ * Project:test-composer
+ * Description:
+ */
+namespace name\space;
+
+class Test{
+
+    public function test_function(){
+        //TODO
+        return 'someone';
+
+    }
+
+}
